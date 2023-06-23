12a1aceab5b4137910358780e1c94fc55c963266
Tejaswini Khairnar <tejaswinikhairnar97@gmail.com>
2023-06-23 14:43:17 +0530

    with autowired

--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("orders")
 public class OrderController {
+    @Autowired
  OrderService orderService;
 
     @PostMapping("/add-order")
